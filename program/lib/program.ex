f77713e1222bb7c7ec8f89beb3a731765c320ead
JuanZea <Juan.Zea@evertecinc.com>
2023-03-13 23:18:40 -0500

    first: commit

---
+++ program/lib/program.ex
@@ -0,0 +1,18 @@
+defmodule Program do
+  @moduledoc """
+  Documentation for `Program`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Program.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
